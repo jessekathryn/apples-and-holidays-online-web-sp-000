f94a51d41005d117a78df250d1c99c1543cdd2a0
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-04-21 01:18:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.find_all do |season, holidays|
+  holiday_hash.collect do |season, holidays|
     holidays.find_all do |holiday, supplies|
      if supplies.include?("BBQ")
       holiday
