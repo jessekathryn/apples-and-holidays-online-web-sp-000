6299a42421662153fca5cf46c145d14c34fe4e30
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-04-21 01:33:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,8 +57,8 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holidays|
-    holidays.find_all do |holiday, supplies|
-      supplies.include?("BBQ") 
+    holidays.find_all do |supplies.inlcude?("BBQ")|
+     # supplies.include?("BBQ") 
       holiday
   end
  end
